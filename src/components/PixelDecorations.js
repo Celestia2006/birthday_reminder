f19af705eae62f159f90822fc2091df3e8f32444
Himasree Gangisetti <himarm2006@gmail.com>
2025-07-09 02:15:28 +0530

    2nd commit

--- src/components/PixelDecorations.js
+++
@@ -1,7 +0,0 @@
-export const PixelFlowers = () => (
-  <div className="pixel-flower" style={{ position: 'absolute', bottom: '20px', left: '40px' }}>
-    <div className="flower-stem" />
-    <div className="flower-head" />
-  </div>
-);
-
