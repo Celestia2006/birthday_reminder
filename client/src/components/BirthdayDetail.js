ab0d5e663d963e0451c6e66f156f43c04f27e813
Himasree Gangisetti <himarm2006@gmail.com>
2025-07-19 01:28:16 +0530

    Edit Birthday Refresh 2

--- client/src/components/BirthdayDetail.js
+++ client/src/components/BirthdayDetail.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate, useLocation } from "react-router-dom";
 import "../styles/BirthdayDetail.css";
 
 const formatPhoneNumber = (num) => {
