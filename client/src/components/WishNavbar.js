ac35916766f62bc25be5fe32a25d2e2246ad07bb
Himasree Gangisetti <himarm2006@gmail.com>
2025-07-20 13:41:59 +0530

    Birthday Wish 10

--- client/src/components/WishNavbar.js
+++ client/src/components/WishNavbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "./WishNavbar.css";
+import "..styles/WishNavbar.css";
 
 const WishNavbar = () => {
   const navigate = useNavigate();
